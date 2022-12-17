1a546bf6bc2a5b4fb62dccde427bd9d26f7401d3
ivan <ivanpwh@gmail.com>
2022-12-17 21:07:54 +0700

    Initialize movie catalogue automated testing implemented

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,13 @@
+const CONFIG = {
+  KEY: 'fd3393c5ee8fe18af0ce6289919f4e51',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+  CACHE_NAME: 'MovieCatalogue-V1',
+  DATABASE_NAME: 'movie-catalogue-database',
+  DATABASE_VERSION: 1,
+  OBJECT_STORE_NAME: 'movies',
+  WEB_SOCKET_SERVER: 'wss://movies-feed.dicoding.dev',
+};
+
+export default CONFIG;
